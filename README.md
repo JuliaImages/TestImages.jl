258adf3ee5bc1cc9a12553219e7f7457f999e9ef
mronian <anchitn@gmail.com>
2016-04-19 02:07:51 +0530

    Updates links in the source and modifies testimages function to check the repository

--- README.md
+++ README.md
@@ -7,6 +7,10 @@ This package provides a convenient Julia interface for loading
 This is to be used in conjunction with the
 [Images](https://github.com/timholy/Images.jl) package.
 
+## Documentation
+
+Full documentation and description of the images available in TestImages.jl can be found [here](http://timholy.github.io/TestImages.jl/).
+
 ## Installation
 
 On Linux and OSX, this should install automatically. If you find yourself missing most of the images described below, please try `Pkg.build("TestImages")`, which should trigger another attempt to download the images.
@@ -26,69 +30,7 @@ inside this package.  Any image file stored in this directory is
 accessible through the `testimage` function.  You can supply the file
 extension (e.g., ".png", ".tif", etc), but it is not required.
 
-The supported images derive from a few publicly-available `zip` files
-or databases. Here is a list of the currently-supported images:
+## Contributing
 
-`autumn_leaves` (color with transparency)
+A detailed guide on contributing to TestImages.jl can be found in the documentation mentioned above.
 
-![autumn_leaves](http://www.imagemagick.org/Usage/images/autumn_leaves.png)
-
-`cameraman`
-
-![cameraman](https://www.math.ust.hk/~masyleung/Teaching/CAS/MATLAB/image/images/cameraman.jpg)
-
-`earth_apollo17` (a particularly large image)
-
-![earth_apollo17](http://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg)
-
-`house`
-
-![house](http://download.visinf.tu-darmstadt.de/data/foe/img/house.png)
-
-`jetplane`
-
-`lake`
-
-![lake](images/lake.tif)
-
-`lena_color_256`, `lena_color_512`, `lena_gray_256`, `lena_gray_512`, `lena_gray_16bit`
-
-![lena_gray_512](http://www.ece.rice.edu/~wakin/images/lenaTest3.jpg)
-
-`lighthouse`
-
-![lighthouse](http://r0k.us/graphics/kodak/kodak/kodim21.png)
-
-`livingroom`
-
-`mandril_color`, `mandril_gray`, `mandrill`
-
-![mandril_color](http://courses.cs.washington.edu/courses/cse455/10wi/projects/p1a/mandrill.png)
-
-`moonsurface`
-
-`mountainstream`
-
-![mountainstream](http://r0k.us/graphics/kodak/kodak/kodim13.png)
-
-`peppers_color`, `peppers_gray`
-
-![peppers_color](http://www.topnews.in/files/Pepper_02.jpg)
-
-`pirate`
-
-`toucan` (8-bit RGBA)
-
-`walkbridge`
-
-`woman_blonde`
-
-`woman_darkhair`
-
-`fabio`
-
-![fabio](images/fabio.png)
-
-The Fabio image was extracted as a counterbalance to Lena from:
-
-Needell, D., and Ward, R. "Stable image reconstruction using total variation minimization." SIAM Journal on Imaging Sciences 6.2 (2013): 1035-1058.
--- deps/build.jl
+++ deps/build.jl
@@ -1,68 +1,43 @@
-using ZipFile, Compat
-
-function expandzip(fname)
-    r = ZipFile.Reader(fname)
-    for f in r.files
-        fn = basename(f.name)
-        if !isempty(fn)
-            open(joinpath(imagedir, fn), "w") do s
-                write(s, readstring(f))
-            end
-        end
-    end
-    close(r)
-    rm(fname)
-end
-
 imagedir = Pkg.dir("TestImages", "images")
 if !isdir(imagedir)
     mkdir(imagedir)
 end
 
-# Determine whether standard_test_images has already been installed
-stdfiles = ["cameraman.tif", "house.tif", "jetplane.tif", "lake.tif"] # just the first several
-have_std = true
-for fname in stdfiles
-    if !isfile(joinpath(imagedir, fname))
-        have_std = false
-        break
-    end
-end
-if !have_std
-    fname = Pkg.dir("TestImages", "images", "standard_test_images.zip")
-    info("Downloading standard test images")
-    download("http://www.imageprocessingplace.com/downloads_V3/root_downloads/image_databases/standard_test_images.zip", fname)
-    expandzip(fname)
-end
+REPO_URL = "https://github.com/timholy/TestImages.jl/blob/gh-pages/images/"
 
-# Additional standard files
-remotedict = @compat Dict(
-    "lighthouse.png" => "http://r0k.us/graphics/kodak/kodak/kodim21.png",
-    "mountainstream.png" => "http://r0k.us/graphics/kodak/kodak/kodim13.png",
-    "moonsurface.tiff" => "http://sipi.usc.edu/database/download.php?vol=misc&img=5.1.09",
-    "mandrill.tiff" => "http://sipi.usc.edu/database/download.php?vol=misc&img=4.2.03",
-    "autumn_leaves.png" => "http://www.imagemagick.org/Usage/images/autumn_leaves.png",
-    "earth_apollo17.jpg" => "http://upload.wikimedia.org/wikipedia/commons/9/97/The_Earth_seen_from_Apollo_17.jpg",
-    "toucan.png" => "http://www.libpng.org/pub/png/img_png/stefan_full_rgba.png",
-    "mri-stack.tif" => "http://imagej.nih.gov/ij/images/mri-stack.zip",
-    "m51.tif" => "http://imagej.nih.gov/ij/images/m51.tif",
-    "hela-cells.tif" => "http://imagej.nih.gov/ij/images/hela-cells.zip",
-    "blobs.gif" => "http://imagej.nih.gov/ij/images/blobs.gif",
-    "multi-channel-time-series.ome.tif" => "http://www.openmicroscopy.org/Schemas/Samples/2015-01/bioformats-artificial/multi-channel-time-series.ome.tif",
-    "lena_gray_16bit.png" => "http://cosmin.users.sourceforge.net/testing/lena_16g_lin.png"
-)
+stdfiles = [
+    "cameraman.tif" ,
+    "house.tif" ,
+    "jetplane.tif" ,
+    "lake.tif" ,
+    "lena_color_256.tif" ,
+    "lena_color_512.tif" ,
+    "lena_gray_256.tif" ,
+    "lena_gray_512.tif" ,
+    "livingroom.tif" ,
+    "mandril_color.tif" ,
+    "mandril_gray.tif" ,
+    "mandrill.tiff" ,
+    "peppers_color.tif" ,
+    "peppers_gray.tif" ,
+    "pirate.tif" ,
+    "walkbridge.tif" ,
+    "woman_blonde.tif" ,
+    "woman_darkhair.tif" ,
+]
 
-for (k, v) in remotedict
-    fn = joinpath(imagedir, k)
+info("Downloading standard test images")
+for f in stdfiles
+    fn = joinpath(imagedir, f)
     if !isfile(fn)
         info("Downloading $fn")
-        # an ugly kluge to deal with urls that point at zipped files
-        _, ext = splitext(v)
-        if ext == ".zip"
-            download(v, joinpath(imagedir, k * ".zip"))
-            expandzip(fn * ".zip")
-        else
-            download(v, joinpath(imagedir, k))
-        end
+        download(REPO_URL*f*"?raw=true", joinpath(imagedir, f))
     end
 end
+
+info("Download Completed.")
+
+
+
+
+
