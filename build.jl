adbde37cbee7508a98fbfcb08c170f1e3d9bf087
Ronny Bergmann <git@ronnybergmann.net>
2019-05-05 15:44:38 +0200

    updates Julia & HTML code to also handle metadata and adds a color version of the lake image.

--- build.jl
+++ build.jl
@@ -1,25 +1,31 @@
-using Images
+using Images, YAML, ColorTypes
+# Open the HTML prequel and put it into our content we build.
 f = open("open.txt")
+content = read(f,String)
 
-content = readstring(f)
-
+# Read all images
 f = readdir("images/")
+# Read yaml metadata
+imageMetadata = YAML.load(open("metadata.yml"))
 
+# Initialize Thumbnails directory and set default height to 200
 if isdir("thumbnails")
-	rm("thumbnails/", recursive=true)
+  rm("thumbnails/", recursive=true)
 end
-
 mkdir("thumbnails")
 cp("cannot_be_displayed.png","thumbnails/cannot_be_displayed.png")
-
 HEIGHT = 200
 
+# Go through all files
 for i in f
+	global content
 	println("Processing "*i)
 	img = load(joinpath("images/",i))
 	fname, ext =splitext(i)
-	file=""
-	if length(size(img))==2
+  file=""
+  # fetch metadata (is empty if no yaml entry exists)
+  fileMeta = get(imageMetadata,fname,Dict())
+  if length(size(img))==2
 		file=fname*".png"
 		if !isfile(joinpath("thumbnails/",file))
 			img_size = size(img)
@@ -29,13 +35,22 @@ for i in f
 	else
 		file="cannot_be_displayed.png"
 	end
-	content *= "\""*fname*"\" : "*"{floc:\""*file*"\" ,col:\""*img.properties["colorspace"]*"\", size : \""*string(size(img))*"\", ftype : \""*uppercase(ext[2:end])*"\"},\n"
+	content *= "\""*fname*"\" : "*"{"
+  # check for metatdata fields and add them
+  for key in ["name,","url","author"]
+    if length(get(fileMeta,key,"")) > 0 # field exists
+      content *= key * ":\"" * get(fileMeta,key,"") * "\", "
+    end
+  end
+  # and the classical ones
+  content *= "file_location:\""*file*"\", "
+  content *= "colorspace:\""*string(base_color_type(eltype(img)))*"\", "
+  content *= "size : \""*string(size(img))*"\", "
+  content *= "filetype : \""*uppercase(ext[2:end])*"\"},\n"
 end
-
+# add end of html file
 f = open("close.txt")
-
-content *= readstring(f)
-
+content *= read(f,String)
 close(f)
 outfile = open("displayimages.js", "w")
 write(outfile,content)
--- displayimages.js
+++ displayimages.js
@@ -2,11 +2,24 @@ function getImageHTML(image,f){
 
 	var elem="<td class=\"text-center\">"
 
-	elem += "<img src=\"thumbnails/"+image["floc"]+"\"class=\"table_image\">"
-	elem += "<h5> Name : <a href=\"images/"+image["floc"]+"\">"+f+"</a></h5>"
-	elem += "<h5> Colour : "+image["col"]+"</h5>"
-	elem += "<h5> File Type : "+image["ftype"]+"</h5>"
-	elem += "<h5> Size : "+image["size"]+"</h5>"
+	elem += "<a href=\"images/" + image["file_location"] +"\"><img src=\"thumbnails/" + image["file_location"] + "\"class=\"table_image\"></a>"
+	if ("name" in image) {
+		elem += "<a href=\"images/" + image["file_location"] + "\">" + image["name"] + "</a><br>"
+		elem +=" <span class=\"filename\">"+f+"<br>"
+	} else { // old style just link file
+		elem += "<a href=\"images/"+image["file_location"]+"\">"+f+"</a><br>"
+	}
+	elem += "<dl>"
+	elem += "<dt>Color</dt><dd>"+image["colorspace"]+"</dd>"
+	elem += "<dt>Type</dt><dd>"+image["filetype"]+"</dd>"
+	elem += "<dt>Size</dt><dd>"+image["size"]+"</dd>"
+	elem += "</dl>"
+	if ("url" in image) {
+		elem += "<span class=\"meta_info\"><a href=\""+image["url"]+"\">source</a></span>"
+	}
+	if ("author" in image) {
+		elem += "<span class=\"meta_info\">" + image["author"]+"</span>"
+  }
 	elem+="</td>"
 
 	return elem;
@@ -15,40 +28,40 @@ function getImageHTML(image,f){
 
 $('document').ready(function(){
 
-	var images = {"autumn_leaves" : {floc:"autumn_leaves.png" ,col:"sRGB", size : "(140,105)", ftype : "PNG"},
-"blobs" : {floc:"blobs.png" ,col:"sRGB", size : "(256,254)", ftype : "GIF"},
-"cameraman" : {floc:"cameraman.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"earth_apollo17" : {floc:"earth_apollo17.png" ,col:"sRGB", size : "(3000,3002)", ftype : "JPG"},
-"fabio_color_256" : {floc:"fabio_color_256.png" ,col:"sRGB", size : "(256,256)", ftype : "PNG"},
-"fabio_color_512" : {floc:"fabio_color_512.png" ,col:"sRGB", size : "(512,512)", ftype : "PNG"},
-"fabio_gray_256" : {floc:"fabio_gray_256.png" ,col:"Gray", size : "(256,256)", ftype : "PNG"},
-"fabio_gray_512" : {floc:"fabio_gray_512.png" ,col:"Gray", size : "(512,512)", ftype : "PNG"},
-"hela-cells" : {floc:"hela-cells.png" ,col:"sRGB", size : "(672,512)", ftype : "TIF"},
-"house" : {floc:"house.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"jetplane" : {floc:"jetplane.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"lake" : {floc:"lake.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"lena_color_256" : {floc:"lena_color_256.png" ,col:"sRGB", size : "(256,256)", ftype : "TIF"},
-"lena_color_512" : {floc:"lena_color_512.png" ,col:"sRGB", size : "(512,512)", ftype : "TIF"},
-"lena_gray_16bit" : {floc:"lena_gray_16bit.png" ,col:"Gray", size : "(256,256)", ftype : "PNG"},
-"lena_gray_256" : {floc:"lena_gray_256.png" ,col:"Gray", size : "(256,256)", ftype : "TIF"},
-"lena_gray_512" : {floc:"lena_gray_512.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"lighthouse" : {floc:"lighthouse.png" ,col:"sRGB", size : "(768,512)", ftype : "PNG"},
-"livingroom" : {floc:"livingroom.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"m51" : {floc:"m51.png" ,col:"Gray", size : "(320,510)", ftype : "TIF"},
-"mandril_color" : {floc:"mandril_color.png" ,col:"sRGB", size : "(512,512)", ftype : "TIF"},
-"mandril_gray" : {floc:"mandril_gray.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"mandrill" : {floc:"mandrill.png" ,col:"sRGB", size : "(512,512)", ftype : "TIFF"},
-"moonsurface" : {floc:"moonsurface.png" ,col:"Gray", size : "(256,256)", ftype : "TIFF"},
-"mountainstream" : {floc:"mountainstream.png" ,col:"sRGB", size : "(768,512)", ftype : "PNG"},
-"mri-stack" : {floc:"cannot_be_displayed.png" ,col:"Gray", size : "(186,226,27)", ftype : "TIF"},
-"multi-channel-time-series.ome" : {floc:"cannot_be_displayed.png" ,col:"Gray", size : "(439,167,21)", ftype : "TIF"},
-"peppers_color" : {floc:"peppers_color.png" ,col:"sRGB", size : "(512,512)", ftype : "TIF"},
-"peppers_gray" : {floc:"peppers_gray.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"pirate" : {floc:"pirate.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"toucan" : {floc:"toucan.png" ,col:"RGB", size : "(162,150)", ftype : "PNG"},
-"walkbridge" : {floc:"walkbridge.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"woman_blonde" : {floc:"woman_blonde.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
-"woman_darkhair" : {floc:"woman_darkhair.png" ,col:"Gray", size : "(512,512)", ftype : "TIF"},
+	var images = {"autumn_leaves" : {name:"Autumn Leaves", author: "A", url:"B",file_location:"autumn_leaves.png" ,colorspace:"sRGB", size : "(140,105)", filetype : "PNG"},
+"blobs" : {file_location:"blobs.png" ,colorspace:"sRGB", size : "(256,254)", filetype : "GIF"},
+"cameraman" : {file_location:"cameraman.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"earth_apollo17" : {file_location:"earth_apollo17.png" ,colorspace:"sRGB", size : "(3000,3002)", filetype : "JPG"},
+"fabio_color_256" : {file_location:"fabio_colorspaceor_256.png" ,colorspace:"sRGB", size : "(256,256)", filetype : "PNG"},
+"fabio_color_512" : {file_location:"fabio_colorspaceor_512.png" ,colorspace:"sRGB", size : "(512,512)", filetype : "PNG"},
+"fabio_gray_256" : {file_location:"fabio_gray_256.png" ,colorspace:"Gray", size : "(256,256)", filetype : "PNG"},
+"fabio_gray_512" : {file_location:"fabio_gray_512.png" ,colorspace:"Gray", size : "(512,512)", filetype : "PNG"},
+"hela-cells" : {file_location:"hela-cells.png" ,colorspace:"sRGB", size : "(672,512)", filetype : "TIF"},
+"house" : {file_location:"house.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"jetplane" : {file_location:"jetplane.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"lake" : {file_location:"lake.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"lena_colorspaceor_256" : {file_location:"lena_colorspaceor_256.png" ,colorspace:"sRGB", size : "(256,256)", filetype : "TIF"},
+"lena_colorspaceor_512" : {file_location:"lena_colorspaceor_512.png" ,colorspace:"sRGB", size : "(512,512)", filetype : "TIF"},
+"lena_gray_16bit" : {file_location:"lena_gray_16bit.png" ,colorspace:"Gray", size : "(256,256)", filetype : "PNG"},
+"lena_gray_256" : {file_location:"lena_gray_256.png" ,colorspace:"Gray", size : "(256,256)", filetype : "TIF"},
+"lena_gray_512" : {file_location:"lena_gray_512.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"lighthouse" : {file_location:"lighthouse.png" ,colorspace:"sRGB", size : "(768,512)", filetype : "PNG"},
+"livingroom" : {file_location:"livingroom.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"m51" : {file_location:"m51.png" ,colorspace:"Gray", size : "(320,510)", filetype : "TIF"},
+"mandril_colorspaceor" : {file_location:"mandril_colorspaceor.png" ,colorspace:"sRGB", size : "(512,512)", filetype : "TIF"},
+"mandril_gray" : {file_location:"mandril_gray.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"mandrill" : {file_location:"mandrill.png" ,colorspace:"sRGB", size : "(512,512)", filetype : "TIFF"},
+"moonsurface" : {file_location:"moonsurface.png" ,colorspace:"Gray", size : "(256,256)", filetype : "TIFF"},
+"mountainstream" : {file_location:"mountainstream.png" ,colorspace:"sRGB", size : "(768,512)", filetype : "PNG"},
+"mri-stack" : {file_location:"cannot_be_displayed.png" ,colorspace:"Gray", size : "(186,226,27)", filetype : "TIF"},
+"multi-channel-time-series.ome" : {file_location:"cannot_be_displayed.png" ,colorspace:"Gray", size : "(439,167,21)", filetype : "TIF"},
+"peppers_colorspaceor" : {file_location:"peppers_colorspaceor.png" ,colorspace:"sRGB", size : "(512,512)", filetype : "TIF"},
+"peppers_gray" : {file_location:"peppers_gray.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"pirate" : {file_location:"pirate.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"toucan" : {file_location:"toucan.png" ,colorspace:"RGB", size : "(162,150)", filetype : "PNG"},
+"walkbridge" : {file_location:"walkbridge.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"woman_blonde" : {file_location:"woman_blonde.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
+"woman_darkhair" : {file_location:"woman_darkhair.png" ,colorspace:"Gray", size : "(512,512)", filetype : "TIF"},
 }
 
 	var create_new_row=0;
