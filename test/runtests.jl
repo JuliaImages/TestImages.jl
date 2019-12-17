3467f586313b65b7826f51a7fcae7c20ccb3465f
Johnny Chen <johnnychen94@hotmail.com>
2019-12-17 08:23:57 +0800

    Test each file in TestImages.remotefiles (#58)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using TestImages, FixedPointNumbers, Colors, AxisArrays
 using Test
 
+# make sure all files in remotefiles are valid image files
+foreach(TestImages.remotefiles) do img_name
+    img = testimage(img_name)
+    @test isa(img, AbstractArray{<:Colorant})
+end
+
 img = testimage("cameraman")
 @test isa(img, Matrix{Gray{N0f8}})
 img = testimage("mri-stack")
