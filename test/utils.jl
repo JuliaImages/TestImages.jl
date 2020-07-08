0c7ae70977dce3b9e059c48bb312bcbae2c81871
Johnny Chen <johnnychen94@hotmail.com>
2020-07-08 18:07:17 +0800

    give better hints and fallback when filename mismatches (#70)

---
+++ test/utils.jl
@@ -0,0 +1,15 @@
+macro except_str(expr, err_type)
+    return quote
+        let err = nothing
+            try
+                $(esc(expr))
+            catch err
+            end
+            err === nothing && error("expected failure, but no exception thrown")
+            @test typeof(err) === $(esc(err_type))
+            buf = IOBuffer()
+            showerror(buf, err)
+            String(take!(buf))
+        end
+    end
+end
