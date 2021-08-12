2e400bc6fe678c47db475cc4ae14b113015cfa86
hyrodium <hyrodium@gmail.com>
2021-08-12 21:19:22 +0900

    update build_artifacts.jl

--- build_artifacts.jl
+++ build_artifacts.jl
@@ -2,9 +2,10 @@
 # it into `Artifacts.toml`.
 # https://github.com/JuliaImages/TestImages.jl/issues/67
 #
-# Github action "Upload Artifacts" runs this script and uploads the generated artifact file
+# GitHub action "Upload Artifacts" runs this script and uploads the generated artifact file
 
 using SHA
+using TOML
 import Tar
 import TranscodingStreams: TranscodingStream
 import CodecZlib: GzipCompressor, GzipDecompressor
@@ -12,15 +13,23 @@ import CodecZlib: GzipCompressor, GzipDecompressor
 compress(io::IO) = TranscodingStream(GzipCompressor(level=9), io)
 decompress(io::IO) = TranscodingStream(GzipDecompressor(), io)
 
-
+# Generate images.tar.gz
 tarball, tree_path = abspath.(("images.tar.gz", "images"))
 open(tarball, write=true) do io
     close(Tar.create(tree_path, compress(io)))
 end
 
+# Check the filenames in images/metadata.toml
+metadata = TOML.parsefile(joinpath("images","metadata.toml"))
+filenames_meta = [img["filename"] for img in metadata["images"]]
+filenames_dir = [splitext(name)[1] for name in readdir("images")]
+@assert filenames_meta ⊆ filenames_dir
+
+# Generate hashes
 tree_hash = open(io -> Tar.tree_hash(decompress(io)), tarball)
 tarball_hash = open(io -> bytes2hex(sha256(io)), tarball)
 
+# Set the hashes as output parameters if on GitHub Actions
 if !haskey(ENV, "GITHUB_ACTIONS")
     @info "Artifact generated" tarball_hash tree_hash
 else
