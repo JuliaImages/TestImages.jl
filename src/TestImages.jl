6052277646c613f0485939fae50627ab7e65d4ab
Tamas Nagy <tamas@tamasnagy.com>
2023-04-07 13:34:00 -0700

    remove forced conversion of mri stack

--- src/TestImages.jl
+++ src/TestImages.jl
@@ -130,7 +130,7 @@ function testimage(filename; download_only::Bool = false, nowarn=false, ops...)
     if basename(imagefile) == "mri-stack.tif"
         # orientation is posterior-right-superior,
         # see http://www.grahamwideman.com/gw/brain/orientation/orientterms.htm
-        return AxisArray(convert(Array{Gray{N0f8},3}, img), (:P, :R, :S), (1, 1, 5))
+        return AxisArray(img, (:P, :R, :S), (1, 1, 5))
     elseif basename(imagefile) == "simple_3d_psf.tif"
         # kernel center is at (0, 0, 0)
         return OffsetArray(convert(Array{Gray{N0f8},3}, img), (-33, -33, -33))
