55f6176f7016ee726184d0c235c9ff2175dc961a
Yuto Horikawa <hyrodium@gmail.com>
2024-10-25 02:57:06 +0000

    build based on e9f084c

--- versions.js
+++ versions.js
@@ -1,7 +1,9 @@
 var DOC_VERSIONS = [
   "stable",
+  "v1.9",
+  "v1.7",
   "v1.6",
   "dev",
 ];
-var DOCUMENTER_NEWEST = "v1.6.0";
+var DOCUMENTER_NEWEST = "v1.9.0";
 var DOCUMENTER_STABLE = "stable";
